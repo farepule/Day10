14882a7b4a905fd5c63d8cc78acc6863e65b2ce1
farepule <faruk.puska@gmail.com>
2021-03-17 23:10:15 +0100

    myPet

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,3 +1,4 @@
+// Only change code below this line
 var myPet = {
     species: "cat",
     name: "cicko",
@@ -7,5 +8,6 @@ var myPet = {
 function myFunction (myObj){
     return myObj;
 }
+// Only change code above this line
 console.log(myFunction(myPet));
 module.exports = { myPet, myFunction };
